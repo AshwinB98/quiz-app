5e79c371e9ee3fdeb9679755d1fde4f45fe6660f
Ashwin Baskaran <ashwinbaskaran151213@gmail.com>
2023-07-13 16:10:59 +0530

    added progress bar component.

---
+++ src/atoms/quiz/progressBar.jsx
@@ -0,0 +1,39 @@
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import { styled } from "styled-components";
+
+const ProgressBar = ({ minValue, maxValue, value }) => {
+  return (
+    <CircularProgressbarWithChildren
+      minValue={minValue}
+      value={value}
+      maxValue={maxValue}
+      styles={buildStyles({
+        strokeLinecap: "round",
+        pathTransitionDuration: 0.5,
+        pathColor: `rgb(68, 183, 123)`,
+        trailColor: "#d6d6d6",
+      })}
+    >
+      <Count>
+        {value}
+        <span>/{maxValue}</span>
+      </Count>
+    </CircularProgressbarWithChildren>
+  );
+};
+
+export default ProgressBar;
+
+const Count = styled.h1`
+  font-size: 2.6rem;
+  margin-top: -18px;
+  font-weight: 900;
+  font-style: italic;
+  span {
+    font-size: 1.125rem;
+    color: #999999;
+  }
+`;
