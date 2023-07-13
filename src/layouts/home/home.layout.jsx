f99652068953e788f59a64eb808e74a46859339f
Ashwin Baskaran <ashwinbaskaran151213@gmail.com>
2023-07-13 13:13:34 +0530

    Added homepage for mobile and desktop view.

--- src/layouts/home/home.layout.jsx
+++ src/layouts/home/home.layout.jsx
@@ -1,9 +1,22 @@
-import { HomeContainer } from "./home.style";
+import { ReactComponent as UpraisedLogo } from "../../assets/home/upraised-logo.svg";
+import Button from "../../atoms/common/button";
+import Title from "../../components/home/title/title.component";
+import { HomeContainer, LeftContainer, RightContainer } from "./home.style";
 
 const Home = () => {
   return (
     <HomeContainer>
-      <p>Hello </p>
+      <LeftContainer>
+        <h1>Upskill yourself.</h1>
+        <h2>You're ready. Now is best time.</h2>
+      </LeftContainer>
+      <RightContainer>
+        <UpraisedLogo />
+        <Title />
+        <Button name={"startBtn"} type={"button"}>
+          START QUIZ
+        </Button>
+      </RightContainer>
     </HomeContainer>
   );
 };
--- src/layouts/home/home.style.js
+++ src/layouts/home/home.style.js
@@ -1,10 +1,58 @@
 import { styled } from "styled-components";
-import { Box } from "../../atoms/common/Box";
+import { Flexbox } from "../../atoms/common/Box";
+import { StyledButton } from "../../atoms/common/button";
 
-export const HomeContainer = styled(Box)`
+export const HomeContainer = styled(Flexbox)`
   height: 100dvh;
   width: 100dvw;
   background: url(${require(`../../assets/home/home-bg.svg`).default}) no-repeat
     bottom;
   background-size: cover;
+  flex-wrap: wrap;
+`;
+
+export const RightContainer = styled(Flexbox)`
+  flex-direction: column;
+  justify-content: space-between;
+
+  align-items: center;
+  width: 100%;
+  padding: 1.25rem;
+  ${StyledButton} {
+    width: 100%;
+  }
+  svg {
+    width: 10rem;
+  }
+  @media only screen and (min-width: 48rem) {
+    width: 50%;
+    background-image: linear-gradient(
+      to right,
+      rgba(255, 0, 0, 0),
+      rgba(255, 255, 255, 0.6)
+    );
+    border-bottom-left-radius: 1.5rem;
+    svg {
+      width: 16rem;
+    }
+  }
+`;
+export const LeftContainer = styled(Flexbox)`
+  display: none;
+  @media only screen and (min-width: 48rem) {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    justify-content: center;
+    /* background-color: white; */
+    padding-left: 2rem;
+    h1 {
+      color: rgb(255, 117, 4);
+      font-size: 5rem;
+    }
+    h2 {
+      font-size: 2.6rem;
+      color: #898b8e;
+    }
+  }
 `;
