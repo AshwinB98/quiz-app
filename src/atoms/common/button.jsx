3525f682982f4360e2bbc3202374c256683d0e5e
Ashwin Baskaran <ashwinbaskaran151213@gmail.com>
2023-07-13 11:19:00 +0530

    created a common button.

---
+++ src/atoms/common/button.jsx
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+
+const Button = ({ children, type, name, disabled }) => {
+  return (
+    <StyledButton disabled name={name} type={type}>
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
+
+const StyledButton = styled.button`
+  padding: 1rem;
+  border: 0;
+  background-color: rgb(255, 117, 4);
+  color: white;
+  font-weight: bold;
+  border-radius: 1.625rem;
+  cursor: pointer;
+  &:disabled {
+    background-color: rgba(171, 172, 174, 0.7);
+    cursor: not-allowed;
+  }
+`;
